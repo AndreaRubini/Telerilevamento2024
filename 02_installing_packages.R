e08a75785ecc4918f920cf17bee47fe0c786c125
AndreaRubini <162352303+AndreaRubini@users.noreply.github.com>
2024-03-07 17:49:14 +0100

    Create 02_installing_packages.R

---
+++ 02_installing_packages.R
@@ -0,0 +1,7 @@
+#Installing new packages in R
+install.packages("terra") #usare sempre le virgolette
+
+library () #carica il pacchetto installato su r serve a cercare eventuali errori
+
+remove.packages () #rimuovere il pacchetto selezionato
+
