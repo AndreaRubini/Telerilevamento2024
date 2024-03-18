aa3bb0c5999d12bdf26c9a8b2e9a34e2e3a0d4f2
AndreaRubini <162352303+AndreaRubini@users.noreply.github.com>
2024-03-18 12:15:53 +0100

    Update 02_installing_packages.R

--- 02_installing_packages.R
+++ 02_installing_packages.R
@@ -1,7 +1,19 @@
-#Installing new packages in R
-install.packages("terra") #usare sempre le virgolette
+# Installing new packages in R
+# Utilizzando la funzione install.packages() si possono installare librerie. Di default R prende le librerie dal CRAN.
+# Lanciando la funzione può richiedere di scegliere la città di provenienza per terminare l'installazione.
+# Si possono installare anche pacchetti sviluppati da programmatori esterni dal CRAN. 
+# Per poter scaricare e utilizzare pacchetti presenti su GitHub bisogna installare il pacchetto devtools presente nel CRAN.
+# Il pacchetto presente su GitHub è imageRy 
 
-library () #carica il pacchetto installato su r serve a cercare eventuali errori
+install.packages("terra")
+install.packages("devtools")
+devtools::install_github("ducciorocchini/imageRy")
+
+# Una volta installato il pacchetto si richiama la funzione library per caricare il pacchetto.
+# Può essere utilizzata anche per controllare se è già presente il pacchetto e non c'è bisogno di utilizzare le virgolette.
+
+library(terra)
+library(devtools)
+library(imageRy)
 
 remove.packages () #rimuovere il pacchetto selezionato
-
