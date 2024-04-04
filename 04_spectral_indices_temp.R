e35f41589570e34d6cca1726bbe7859a4aa2f7d1
AndreaRubini <162352303+AndreaRubini@users.noreply.github.com>
2024-04-04 17:35:22 +0200

    Update 04_spectral_indices_temp.R

--- 04_spectral_indices_temp.R
+++ 04_spectral_indices_temp.R
@@ -70,4 +70,50 @@ plot(ndvi2006, col=clvir)
 ndvi2006a <- im.ndvi(m2006, 1, 2)
 plot(ndvi2006a, col=cl)
 
+#Vegetation index
+#different vegetation index Nir-Red= DVI
+# altissimo se vi è vegetazione bassissimo se non vi è vegetazione
 
+# DVI = NIR - RED
+# bands: 1=NIR, 2=RED, 3=GREEN
+#Calcolo indice DVI in R 
+dvi1992 = m1992[[1]] - m1992[[2]]
+
+plot(dvi1992)
+
+#il simbolo $ serve in R per legare due oggetti insieme.
+#4bit= 16 valori possibili quindi da -15 a + 15 
+#8bit = indice quindi varia in un intervallo da -255 a + 255 (questo indice è quindi variabile in base alla risoluzione radiometrica in entrata: in questo caso 8 Bit
+
+# variazione Palette per migliorare la visualizzazione  
+cl <- colorRampPalette(c("darkblue", "yellow", "red", "black")) (100)
+plot(dvi1992, col=cl)
+
+# calcolo DVI 2006
+# importiamo l'immagine 2006 
+m2006<-im.import("matogrosso_ast_2006209_lrg.jpg")
+
+dvi2006=m2006[[1]]-m2006[[2]]
+
+# plottiamo il risultato con colore cl
+
+ plot(dvi2006,col=cl)
+
+#plot dvi1996 di fianco dvi2006
+par(mfrow=c(1,2))
+plot(dvi1992,col=cl)
+plot(dvi2006,col=cl)
+
+#NDVI (normalizzare i dati) normalize difference vegetation index 
+# es immg 8bit nir-red/ diviso nir+red 255-0/255+0 = 1 0-255/0+255=-1
+#faccio la stessa cosa anche con l'immagine a 4 bit  il valore è un indice tra meno uno e uno 
+ndvi1992<-dvi1992 / (m1992[[1]]+ m1992[[2]])
+ndvi2006<-dvi2006 / (m2006[[1]]+ m2006[[2]])
+
+plot(ndvi2006,col=cl)
+par(mfrow=c(1,2))
+plot(ndvi1992,col=cl)
+plot(ndvi2006,col=cl)
+#opzione per creare la funzione ndvi anzichè fare il calcolo 
+ndvi2006a <- im.ndvi(m2006, 1, 2)
+plot(ndvi2006a, col=cl)
