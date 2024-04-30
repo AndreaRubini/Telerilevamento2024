19c8b76e1c964e3bc75dd8250139abe0f82f9409
AndreaRubini <162352303+AndreaRubini@users.noreply.github.com>
2024-04-30 17:28:35 +0200

    Update 08_variability.R

--- 08_variability.R
+++ 08_variability.R
@@ -2,7 +2,7 @@
 #variabilità--> deviazione standard:  scarto, lo elevo al quadrato
 library(imageRy)
 library(terra)
-# install.packages("viridis")
+# install.packages("viridis") pacchetto per creare grafici che possano essere visibili anche da persone afette da daltonismo
 library(viridis)
 
 im.list()
@@ -14,20 +14,22 @@ im.plotRGB(sent, 1, 2, 3)
 # NIR = band 1
 # red = band 2
 # green = band 3
-
+# creo una finestrella di tre pixel che scansionerà l'immagine al cui interno farò la deviazione standard 
 im.plotRGB(sent, r=2, g=1, b=3)
 
-nir <- sent[[1]]
+nir <- sent[[1]] #nir ora equivale all'oggetto1 
 cl <- colorRampPalette(c("black", "blue", "green", "yellow"))(4)
 plot(nir, col=cl)
-
+#statistiche all'interno di un gruppo di valori sd3 
+#focal = calcolo deviazione standard in questo caso della finestra 3x3
 sd3 <- focal(nir, matrix(1/9, 3, 3), fun=sd)
 plot(sd3)
-
+#utilizzeremo la palette viridis già presente all'interno del pacchetto omonimo
+#il pacchetto 7 all'interno di viridis sono le legende, per averle visualizzate quindi dobbiamo richiamare il pacchetto 7 
 viridisc <- colorRampPalette(viridis(7))(256)
 plot(sd3, col=viridisc)
 
-# Standard deviation 7x7
+# Standard deviation 7x7 
 sd7 <- focal(nir, matrix(1/49, 7, 7), fun=sd)
 plot(sd7, col=viridisc)
 
@@ -40,3 +42,4 @@ sd13 <- focal(nir, matrix(1/169, 13, 13), fun=sd)
 
 stacksd <- c(sd3, sd7, sd13)
 plot(stacksd, col=viridisc)
+#la deviazione standard è un valore relativo rispetto alla riflettanza originale  che va (da 0 a 255) 
